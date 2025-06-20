b633c68663cb922187329497f2b82b2e471f0fe7
Ibrahim Aziz <ibraziz21@gmail.com>
2025-06-20 19:21:53 +0300

    Update page.tsx

--- packages/react-app/app/settings/page.tsx
+++ packages/react-app/app/settings/page.tsx
@@ -3,6 +3,7 @@
 import { Toaster } from '@/components/ui/sonner'
 import { useWeb3 } from '@/contexts/useWeb3'
 import { Chats, Copy, Envelope, Export, GithubLogo, TwitterLogo } from '@phosphor-icons/react/dist/ssr'
+import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import { toast } from 'sonner'
 import truncateEthAddress from "truncate-eth-address";
@@ -51,7 +52,7 @@ const SettingsPage = () => {
             <div className="flex justify-between items-start shadow-lg rounded-xl p-4 text-[#00000080] my-2 bg-white">
                 <Envelope size={24} className="mr-2" color="#219653" />
                 <div className="flex justify-between items-center w-full">
-                    <h2 className="font-medium text-black">Support@minimiles.co</h2>
+                    <h2 className="font-medium text-black">hello@minimiles.app</h2>
                     <Copy size={24} className="cursor-pointer" onClick={() => handleCopy('Support@minimiles.co')} />
                 </div>
             </div>
@@ -60,7 +61,9 @@ const SettingsPage = () => {
                 <Chats size={24} className="mr-2" color="#219653" />
                 <div className="flex justify-between items-center w-full">
                     <h2 className="font-medium text-black">Chat with us</h2>
+                    <Link href='https://t.me/+oRfjEWyA4zo5NTRk'>
                     <Export size={24} />
+                    </Link>
                 </div>
             </div>
 
@@ -68,7 +71,9 @@ const SettingsPage = () => {
                 <TwitterLogo size={24} className="mr-2" color="#219653" />
                 <div className="flex justify-between items-center w-full">
                     <h2 className="font-medium text-black">Message us</h2>
+                    <Link href='https://x.com/minimilesapp'>
                     <Export size={24} />
+                    </Link>
                 </div>
             </div>
 
@@ -78,7 +83,9 @@ const SettingsPage = () => {
                 <GithubLogo size={24} className="mr-2" color="#219653" />
                 <div className="flex justify-between items-center w-full">
                     <h2 className="font-medium text-black">View open source code</h2>
+                    <Link href='https://github.com/ibraziz21/MiniMiles'>
                     <Export size={24} />
+                    </Link>
                 </div>
             </div>
             <div className="mt-4 flex space-x-4 text-sm text-[#00000080]">
