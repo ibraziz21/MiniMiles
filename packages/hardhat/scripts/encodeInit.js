5193fc2c42efec8fc15fdd992d459394c481a24b
Ibrahim Aziz <ibraziz21@gmail.com>
2025-07-03 06:59:25 +0300

    Initialize subgraph

--- packages/hardhat/scripts/encodeInit.js
+++ packages/hardhat/scripts/encodeInit.js
@@ -5,10 +5,10 @@ async function main() {
     "function initialize(address,address,address,address)",
   ]);
   const data = iface.encodeFunctionData("initialize", [
-    '0xb0012Ff26b6eB4F75d09028233204635c0332050',
+    '0xEeD878017f027FE96316007D0ca5fDA58Ee93a6b',
    "0x765de816845861e75a25fca122bb6898b8b1282a",
     '0x48065fbBE25f71C9282ddf5e1cD6D6A887483D5e',
-    '0xa5065676D5d12b202dF10f479F2DDD62234b91b9'])
+    '0xf20a5e1a4ca28d64f2c4a90998a41e8045288f48'])
   console.log(data);
 }
 main();
