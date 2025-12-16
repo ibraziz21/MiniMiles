9d7109f5d5e094f289271dacfc036ddc492e973f
Ibrahim Aziz <ibraziz21@gmail.com>
2025-12-16 10:43:14 +0300

    update page

--- packages/react-app/app/page.tsx
+++ packages/react-app/app/page.tsx
@@ -340,7 +340,7 @@ export default function Home() {
       {/* TOKEN / Join Rewards */}
       <div className="mx-4 mt-6">
         <div className="flex justify-between items-center">
-          <h3 className="text-lg font-medium">Join Rewards</h3>
+          <h3 className="text-lg font-medium">Digital Cash Prizes</h3>
           <Link href="/spend">
             <span className="text-sm text-[#238D9D] hover:underline">
               View more ›
