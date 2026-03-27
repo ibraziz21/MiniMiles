2df8ef0c8f513245a19985efd1412e3da4e3417b
Ibraziz21 <ibraziz21@gmail.com>
2026-03-27 18:25:37 +0300

    update 7 day

--- packages/react-app/components/daily-challenge.tsx
+++ packages/react-app/components/daily-challenge.tsx
@@ -44,8 +44,9 @@ const KILN_DAILY_HOLD_QUEST_ID =
 async function claimSevenDayStreak(addr: string) {
   const res = await fetch("/api/quests/seven_day_streak", {
     method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      userAddress: addr,
+      userAddress: addr.toLowerCase(),
       questId: "6ddc811a-1a4d-4e57-871d-836f07486531",
     }),
   }).then((r) => r.json());
--- packages/react-app/helpers/claimSevenDayStreak.ts
+++ packages/react-app/helpers/claimSevenDayStreak.ts
@@ -1,11 +1,11 @@
 async function claimSevenDayStreak(addr: string) {
-    const res = await fetch("/api/quests/seven_day_streak", {
-      method: "POST",
-      body: JSON.stringify({
-        userAddress: addr,
-        questId: "6ddc811a-1a4d-4e57-871d-836f07486531", // 👈 create this quest in Supabase with this id
-      }),
-    }).then((r) => r.json());
-    return res;
-  }
-  
+  const res = await fetch("/api/quests/seven_day_streak", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      userAddress: addr.toLowerCase(),
+      questId: "6ddc811a-1a4d-4e57-871d-836f07486531",
+    }),
+  }).then((r) => r.json());
+  return res;
+}
